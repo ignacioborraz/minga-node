2b097f1c16b7823f819b16583d9455063f0b663c
ignacioborraz <ignacioborraz@hotmail.com>
2023-05-24 10:41:13 -0300

    router

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import 'dotenv/config.js'
 import './config/database.js'
+import indexRouter from './router/index.js'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const server = express()
 const PORT = process.env.PORT || 8080
@@ -8,6 +11,11 @@ const ready = ()=>console.log('server running on port '+PORT)
 
 //middlewares
 server.use(express.json())                              //permite entradas y también trabajar con formato json
-server.use(express.urlencoded({ extended: false }))     //permite capturar consultas complejas
+server.use(express.urlencoded({ extended:true }))       //permite capturar consultas complejas
+server.use(cors())                                      //para permitir orígenes cruzados (front/back)
+server.use(morgan('dev'))                               //para registrar peticiones HTTP
+
+//router
+server.use('/api',indexRouter)
 
 server.listen(PORT,ready)
