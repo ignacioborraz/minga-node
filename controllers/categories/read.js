9c4a6e1530c6c3b61a7edd114fa3f5bb438b630f
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-23 19:34:43 -0300

    fix

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -15,6 +15,7 @@ export default async(req,res)=> {
             })
         }        
     } catch (error) {
+        console.log(error)
         return res.status(500).json({
             success: false,
             message: 'error'
