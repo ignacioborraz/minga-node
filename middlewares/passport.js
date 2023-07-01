035a35cf260b0e39bde50e986c733d8f07ee3007
ignacioborraz <ignacioborraz@hotmail.com>
2023-07-01 16:16:44 -0300

    fix

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -11,9 +11,8 @@ export default passport.use(
         async (jwt_payload,done) => {
             try {				
                 let user = await User.findOne({_id:jwt_payload._id})
-                delete user._id
-                delete user.password
-                if (user) {		
+                if (user) {
+                    delete user.password
                     return done(null, user)
                 } else {
                     return done(null, false)
